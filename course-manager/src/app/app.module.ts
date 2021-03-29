46f026ef291fbbdb84f14224772b14750d8c44a2
felipeghidini <lipeghidini18@gmail.com>
2021-03-29 16:01:51 -0300

    Criado componente de Lista de cursos

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { CourseListComponent } from './courses/course-list/course-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
