6d91c207bec428276731b18b5fb975a8716b4eb6
felipeghidini <lipeghidini18@gmail.com>
2021-04-02 06:12:06 -0300

    Configurando rotas e pontuação de estrelas

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,24 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { CourseListComponent } from './courses/course-list/course-list.component';
-import { StarComponent } from './star/star/star.component';
+import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Erro404Component } from './erro404/erro404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Erro404Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: Erro404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
